9d1cbc2a4764d94e4c73a1ab9e56b4c99c11a4a3
Iordan <iamironadicted@gmail.com>
2023-03-09 22:05:41 +0200

    finishing touches - HOME page

--- src/components/navigation/burgerNav/BurgerNav.js
+++ src/components/navigation/burgerNav/BurgerNav.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 import "./BurgerNav.css";
 
-const BurgerNav = (props) => {
+const BurgerNav = ({ className, hasUser }) => {
   const [isShown, setIsShown] = useState(false);
 
   const showBurgerMenu = (e) => {
@@ -15,7 +15,7 @@ const BurgerNav = (props) => {
     isShown == true ? "burger-container-show" : "burger-container-hide";
 
   return (
-    <div className={props.className}>
+    <div className={className}>
       {isShown == true ? (
         <FontAwesomeIcon
           icon={faClose}
@@ -29,33 +29,41 @@ const BurgerNav = (props) => {
           onClick={showBurgerMenu}
         />
       )}
-
+      {/* hasUser */}
       <div className={classShown}>
-        <ul className="links-burger">
-          {/* USER */}
-          {/* <li>
-                <a href="#">Custom Workout</a> 
-              </li> */}
-          {/* <li>
-                <a href="#">Custom Foods plan</a>
-              </li> */}
-          {/* GUEST */}
-          <li>
-            <a href="#">Nutrition</a>
-          </li>
-          <li>
-            <a href="#">Workouts</a>
-          </li>
-          <li>
-            <a href="#">Login</a> {/* /exercises */}
-          </li>
-          <li>
-            <a href="#">Register</a> {/* /exercises */}
-          </li>
-          {/* <li>
-                <a href="#">LogOut</a> 
-              </li> */}
-        </ul>
+        {hasUser ? (
+          <ul className="links-burger">
+            {/* USER */}
+            <li>
+              <a href="#">Create Your Own</a>
+            </li>
+            <li>
+              <a href="#">Nutrition</a>
+            </li>
+            <li>
+              <a href="#">Workouts</a>
+            </li>
+            <li>
+              <a href="#">LogOut</a>
+            </li>
+          </ul>
+        ) : (
+          <ul className="links-burger">
+            {/* GUEST */}
+            <li>
+              <a href="#">Nutrition</a>
+            </li>
+            <li>
+              <a href="#">Workouts</a>
+            </li>
+            <li>
+              <a href="#">Login</a> {/* /exercises */}
+            </li>
+            <li>
+              <a href="#">Register</a> {/* /exercises */}
+            </li>
+          </ul>
+        )}
       </div>
     </div>
   );
