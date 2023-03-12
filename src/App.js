9f30590139ae528321afb04084f06e48711f9906
Iordan <iamironadicted@gmail.com>
2023-03-12 12:20:06 +0200

    Adding Login Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./components/navigation/Navbar";
-import Home from "./pages/home/Home";
+// import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
 import Footer from "./components/footer/Footer";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <Navbar hasUser={hasUser} />
-      <Home />
+      <Login />
       <Footer />
     </>
   );
---
+++ src/pages/register/Register.js
@@ -0,0 +1,45 @@
+import styles from "./Register.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faLock } from "@fortawesome/free-solid-svg-icons";
+
+const Register = () => {
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["picture-container"]}>
+        <h1>Welcome Back!</h1>
+        <p>You can sign in to access with your existing account.</p>
+      </div>
+      <form className={styles["form"]}>
+        <h1>Sign In</h1>
+        <div className={styles["input-container"]}>
+          <div className={styles["input-field"]}>
+            <FontAwesomeIcon icon={faUser} className={styles["icon"]} />
+            <input type="email" id="email" name="email" placeholder="Email" />
+          </div>
+          <div className={styles["input-field"]}>
+            <FontAwesomeIcon icon={faLock} className={styles["icon"]} />
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+        </div>
+        <div className={styles["additional"]}>
+          <div className={styles["checkbox"]}>
+            <input type="checkbox" name="remember" id="remember" />
+            <label htmlFor="remember">Remember me</label>
+          </div>
+          <a href="#">Forgot password?</a>
+        </div>
+        <input className={styles["btn-submit"]} type="submit" value="Sign In" />
+        <p className={styles["new-here"]}>
+          New here? <a href="#">Create an Account</a>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
