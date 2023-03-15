d8321878afadccbefe38d09d426113efcd3e1032
Iordan <iamironadicted@gmail.com>
2023-03-15 19:40:08 +0200

    Fixing the Nav and making it work

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,18 +11,18 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className={styles["footer"]}>
-      <Link to="https://github.com/iordanGrancharov/gym-app" target="_blank">
-        <FontAwesomeIcon icon={faGithub} className={styles["icon"]} />
-      </Link>
-      <a href="#">
-        <FontAwesomeIcon icon={faFacebook} className={styles["icon"]} />
-      </a>
-      <Link
-        to="https://www.linkedin.com/in/yordan-gruncharov-151815244"
-        target="_blank"
-      >
-        <FontAwesomeIcon icon={faLinkedin} className={styles["icon"]} />
-      </Link>
+      <p className={styles["text"]}>@Copyright 2023 - All rights reserved</p>
+      <div className={styles["links-continer"]}>
+        <Link to="https://github.com/iordanGrancharov/gym-app" target="_blank">
+          <FontAwesomeIcon icon={faGithub} className={styles["icon"]} />
+        </Link>
+        <Link
+          to="https://www.linkedin.com/in/yordan-gruncharov-151815244"
+          target="_blank"
+        >
+          <FontAwesomeIcon icon={faLinkedin} className={styles["icon"]} />
+        </Link>
+      </div>
     </div>
   );
 };
