051961bea01d4f508409cdc4620da92cef6499dc
Iordan <iamironadicted@gmail.com>
2023-03-22 20:28:03 +0200

    setting up firebase

---
+++ src/firebase/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_RAPID_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_RAPID_FIREBASE_AUTHDOMAIN,
+  databaseURL: process.env.REACT_APP_RAPID_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_RAPID_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_RAPID_FIREBASE_STORAGE,
+  messagingSenderId: process.env.REACT_APP_RAPID_FIREBASE_MESSEGING_SENDER_ID,
+  appId: process.env.REACT_APP_RAPID_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_RAPID_FIREBASE_MEASURMENT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = app.auth();
+export default app;
